c3dc93c230c3370a8b645cd8b0f7dbb6ff492238
Mitch <mitchchiles@gmail.com>
2021-08-23 18:35:04 -0400

    Add splitting messages for long messages in command reply service

--- src/logic/services/command-reply.service.ts
+++ src/logic/services/command-reply.service.ts
@@ -2,82 +2,102 @@
 import { InteractionReplyOptions } from "discord.js";
 import { CommandUserInput, CommandInputContext } from "../../models/Command";
 import { CommandReplyOptions } from "../../models/CommandReply";
+import { StringHelper } from "../helpers/string.helper";
 
 export class CommandReplyService {
+    private maxTextMessageLength: number;
+
+    public constructor(maxTextMessageLength: number = 1900) {
+        this.maxTextMessageLength = maxTextMessageLength;
+    }
+
     public async reply(input: CommandUserInput, options: CommandReplyOptions): Promise<void> {
         if (!input) {
             return;
         }
 
-        if (input.inputContext === CommandInputContext.message) {
-            if (input.msg) {
-                if (options.ephemeral) {
-                    await input.msg.member.send({
-                        content: options.content,
-                        embeds: options.embeds,
-                        files: options.files,
-                        allowedMentions: options.mentions,
-                        components: options.interactionComponents,
-                    });
-                } else {
-                    await input.msg.reply({ 
-                        content: options.content,
-                        embeds: options.embeds,
-                        files: options.files,
-                        allowedMentions: options.mentions,
-                        components: options.interactionComponents,
-                    });
-                }
-            }
-        } 
+        let stringHelper: StringHelper = new StringHelper();
+        let contents: string[];
+        if (options.content) {
+            contents = stringHelper.splitStringIfLengthExceeds(options.content, this.maxTextMessageLength);
+        } else {
+            contents = [''];
+        }
         
-        if (input.inputContext === CommandInputContext.interaction) {
-            if (input.interaction) {
-                let interactionAny: any = <any>input.interaction;
-
-                if (options.wasDeferred) {
-                    if (interactionAny.editReply) {
-                        let replyOptions: InteractionReplyOptions = {
-                            content: options.content,
+        let isFirst: boolean = true;
+        for (let content of contents) {
+            if (input.inputContext === CommandInputContext.message) {
+                if (input.msg) {
+                    if (options.ephemeral) {
+                        await input.msg.member.send({
+                            content: content,
                             embeds: options.embeds,
                             files: options.files,
                             allowedMentions: options.mentions,
                             components: options.interactionComponents,
-                        };
-                        await interactionAny.editReply(replyOptions);
+                        });
                     } else {
-                        throw 'Interaction does not have a editReply function';
+                        await input.msg.reply({ 
+                            content: content,
+                            embeds: options.embeds,
+                            files: options.files,
+                            allowedMentions: options.mentions,
+                            components: options.interactionComponents,
+                        });
                     }
-                } else if (options.isFollowUp) {
-                    if (interactionAny.followUp) {
-                        let replyOptions: InteractionReplyOptions = {
-                            content: options.content,
-                            embeds: options.embeds,
-                            files: options.files,
-                            allowedMentions: options.mentions,
-                            components: options.interactionComponents,
-                            ephemeral: options.ephemeral
-                        };
-                        await interactionAny.followUp(replyOptions);
+                }
+            } 
+            
+            if (input.inputContext === CommandInputContext.interaction) {
+                if (input.interaction) {
+                    let interactionAny: any = <any>input.interaction;
+    
+                    if (options.wasDeferred && isFirst) {
+                        if (interactionAny.editReply) {
+                            let replyOptions: InteractionReplyOptions = {
+                                content: content,
+                                embeds: options.embeds,
+                                files: options.files,
+                                allowedMentions: options.mentions,
+                                components: options.interactionComponents,
+                            };
+                            await interactionAny.editReply(replyOptions);
+                        } else {
+                            throw 'Interaction does not have a editReply function';
+                        }
+                    } else if (options.isFollowUp || !isFirst) {
+                        if (interactionAny.followUp) {
+                            let replyOptions: InteractionReplyOptions = {
+                                content: content,
+                                embeds: options.embeds,
+                                files: options.files,
+                                allowedMentions: options.mentions,
+                                components: options.interactionComponents,
+                                ephemeral: options.ephemeral
+                            };
+                            await interactionAny.followUp(replyOptions);
+                        } else {
+                            throw 'Interaction does not have a followUp function';
+                        }
                     } else {
-                        throw 'Interaction does not have a followUp function';
-                    }
-                } else {
-                    if (interactionAny.reply) {
-                        let replyOptions: InteractionReplyOptions = {
-                            content: options.content,
-                            embeds: options.embeds,
-                            files: options.files,
-                            allowedMentions: options.mentions,
-                            components: options.interactionComponents,
-                            ephemeral: options.ephemeral
-                        };
-                        await interactionAny.reply(replyOptions);
-                    } else {
-                        throw 'Interaction does not have a reply function';
+                        if (interactionAny.reply) {
+                            let replyOptions: InteractionReplyOptions = {
+                                content: content,
+                                embeds: options.embeds,
+                                files: options.files,
+                                allowedMentions: options.mentions,
+                                components: options.interactionComponents,
+                                ephemeral: options.ephemeral
+                            };
+                            await interactionAny.reply(replyOptions);
+                        } else {
+                            throw 'Interaction does not have a reply function';
+                        }
                     }
                 }
             }
+
+            isFirst = false;
         }
     }
 
