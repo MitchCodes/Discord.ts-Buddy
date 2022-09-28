f50844a007b271a31071f5fd84cd6bf51fca4cab
Mitch <mitchchiles@gmail.com>
2022-09-28 17:43:58 -0400

    Update simple reply service

--- src/logic/services/command-simplereply.service.ts
+++ src/logic/services/command-simplereply.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { CommandUserInput } from "../../models/Command";
-import { CommandReplyOptions } from "../../models/CommandReply";
+import { CacheType, Interaction } from "discord.js";
+import { CommandInputContext, CommandUserInput } from "../../models/Command";
+import { CommandReplyOptions, CommandReplyStateOptions } from "../../models/CommandReply";
 import { CommandReplyService } from "./command-reply.service";
 
 export class CommandSimpleReplyService {
@@ -12,22 +13,32 @@ export class CommandSimpleReplyService {
         this.commandReplyService = new CommandReplyService(maxTextMessageLength);
     }
 
-    public async deferReply(input: CommandUserInput, ephemeral: boolean = false): Promise<void> {
-        await this.commandReplyService.deferReply(input, ephemeral);
+    public async deferReply(input: Interaction<CacheType>, ephemeral: boolean = false): Promise<void> {
+        let commandInput: CommandUserInput = new CommandUserInput(CommandInputContext.interaction, null, input);
+        return this.deferReplyHybrid(commandInput, ephemeral);
+    }
+
+    public async deferReplyHybrid(input: CommandUserInput, ephemeral: boolean = false): Promise<void> {
+        await this.commandReplyService.deferReplyHybrid(input, ephemeral);
         this.isDeferred = true;
     }
 
-    public async reply(input: CommandUserInput, options: CommandReplyOptions): Promise<void> {
+    public async reply(input: Interaction<CacheType>, options: CommandReplyOptions): Promise<void> {
+        let commandInput: CommandUserInput = new CommandUserInput(CommandInputContext.interaction, null, input);
+        return this.replyHybrid(commandInput, options);
+    }
+
+    public async replyHybrid(input: CommandUserInput, options: CommandReplyOptions): Promise<void> {
         if (!this.isReplied) {
             if (this.isDeferred) {
-                await this.commandReplyService.replyAfterDefer(input, {...options, wasDeferred: true, isFollowUp: false});
+                await this.commandReplyService.replyAfterDeferHybrid(input, {...options, wasDeferred: true, isFollowUp: false});
             } else {
-                await this.commandReplyService.reply(input, {...options, wasDeferred: false, isFollowUp: false});
+                await this.commandReplyService.replyHybrid(input, {...options, wasDeferred: false, isFollowUp: false});
             }
 
             this.isReplied = true;
         } else {
-            await this.commandReplyService.followUp(input, {...options, wasDeferred: false, isFollowUp: true});
+            await this.commandReplyService.followUpHybrid(input, {...options, wasDeferred: false, isFollowUp: true});
         }
     }
 }
