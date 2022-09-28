2fd0c7bb93b36dfb800b179685ec8d4cd8947522
Mitch <mitchchiles@gmail.com>
2022-09-28 18:22:59 -0400

    Add hybrid support to UserInputHelper

--- src/logic/helpers/user-input.helper.ts
+++ src/logic/helpers/user-input.helper.ts
@@ -1,8 +1,13 @@
-import { Guild, GuildMember, StageChannel, TextBasedChannel, VoiceChannel } from "discord.js";
+import { CacheType, Guild, GuildMember, Interaction, StageChannel, TextBasedChannel, VoiceBasedChannel, VoiceChannel } from "discord.js";
 import { CommandInputContext, CommandUserInput } from "../../models/Command";
 
 export class UserInputHelper {
-    public getGuildMember(input: CommandUserInput): GuildMember {
+    public getGuildMember(input: Interaction<CacheType>): GuildMember {
+        let commandInput: CommandUserInput = new CommandUserInput(CommandInputContext.interaction, null, input);
+        return this.getGuildMemberHybrid(commandInput);
+    }
+
+    public getGuildMemberHybrid(input: CommandUserInput): GuildMember {
         if (input.inputContext === CommandInputContext.message) {
             return input.msg?.member;
         } else if (input.inputContext === CommandInputContext.interaction) {
@@ -17,7 +22,12 @@ export class UserInputHelper {
         }
     }
 
-    public getChannel(input: CommandUserInput): TextBasedChannel {
+    public getChannel(input: Interaction<CacheType>): TextBasedChannel {
+        let commandInput: CommandUserInput = new CommandUserInput(CommandInputContext.interaction, null, input);
+        return this.getChannelHybrid(commandInput);
+    }
+
+    public getChannelHybrid(input: CommandUserInput): TextBasedChannel {
         if (input.inputContext === CommandInputContext.message) {
             return input.msg?.channel;
         } else if (input.inputContext === CommandInputContext.interaction) {
@@ -27,7 +37,12 @@ export class UserInputHelper {
         }
     }
 
-    public getCurrentVoiceChannel(input: CommandUserInput): (VoiceChannel | StageChannel) {
+    public getCurrentVoiceChannel(input: Interaction<CacheType>): VoiceBasedChannel {
+        let commandInput: CommandUserInput = new CommandUserInput(CommandInputContext.interaction, null, input);
+        return this.getCurrentVoiceChannelHybrid(commandInput);
+    }
+
+    public getCurrentVoiceChannelHybrid(input: CommandUserInput): VoiceBasedChannel {
         if (input.inputContext === CommandInputContext.message) {
             return input.msg?.member?.voice?.channel;
         } else if (input.inputContext === CommandInputContext.interaction) {
@@ -42,7 +57,12 @@ export class UserInputHelper {
         }
     }
 
-    public getGuild(input: CommandUserInput): Guild {
+    public getGuild(input: Interaction<CacheType>): Guild {
+        let commandInput: CommandUserInput = new CommandUserInput(CommandInputContext.interaction, null, input);
+        return this.getGuildHybrid(commandInput);
+    }
+
+    public getGuildHybrid(input: CommandUserInput): Guild {
         if (input.inputContext === CommandInputContext.message) {
             return input.msg?.guild;
         } else if (input.inputContext === CommandInputContext.interaction) {
