f83b9c36dd35fa73562526f38e073bf645a9ebfe
Mitch <mitchchiles@gmail.com>
2022-09-29 18:51:58 -0400

    Fix bug

--- src/logic/bots/multi-guild-bot.ts
+++ src/logic/bots/multi-guild-bot.ts
@@ -120,7 +120,7 @@ export class MultiGuildBot implements IDiscordBot, IAutoManagedBot, ICommandSett
             this.commands.push(command);
         }
 
-        if (this.addSettingsCommand && this.commandSettings) {
+        if (this.addSettingsCommand && this.commandSettings && this.commandSettings.length > 0) {
             let settingCommand: SettingsCommand = new SettingsCommand('Settings', 'Settings for ' + this.name, this.logger, this.conf, this, this.commandSettings, this.getSettingsCallback, this.saveSettingCallback);
             this.commands.push(settingCommand);
         }
